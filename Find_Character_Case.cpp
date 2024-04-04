fc703dfc06240ba9ad352296a750654e9b54f2d6
Mostafa Moin <sa479374@gmail.com>
2024-04-04 10:47:16 +0600

    solution

---
+++ Find_Character_Case.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+typedef vector<int> vi;
+typedef pair<int, int> pi;
+typedef vector<pair<int, int>> vip;
+
+#define w(x)            int x;cin>>x;while(x--)
+#define Mostafa         ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define ll              long long
+#define pb              push_back
+#define ff              first
+#define ss              second
+#define mp              make_pair
+#define REP(i,a,b) for (int i = a; i <= b; i++)
+#define ps(x,y)         fixed<<setprecision(y)<<x
+#define setbits(x)      __builtin_popcountll(x)
+#define zrobits(x)      __builtin_ctzll(x)
+
+int main() {
+
+    Mostafa
+
+    char ch; cin >> ch;
+
+    if (ch > 96) {
+        cout << 0 << endl;
+    } else if (ch > 64) {
+        cout << 1 << endl;
+    } else {
+        cout << -1 << endl;
+    }
+
+    return 0;
+}
