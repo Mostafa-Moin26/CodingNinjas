f9033e4944e1342fa86372bfe1bad52781474ce5
Mostafa Al Moin <sa479374@gmail.com>
2025-02-02 11:01:33 +0600

    Minimum Jumps

---
+++ Minimum jumps.cpp
@@ -0,0 +1,33 @@
+// Problem link ----->
+https://www.naukri.com/code360/problems/minimum-jumps_1062693?interviewProblemRedirection=true&search=Minimum%20Jumps
+
+// Solutions ----->
+// O(n) time and O(1) space
+int minimumJumps(vector<int> &arr, int n)
+{
+    // write your code here
+    if (n <= 1) return 0;
+    if (arr[0] == 0) return -1;
+
+    int maxReach = arr[0];
+    int steps = arr[0];
+    int jumps = 1;
+
+    for (int i = 1; i < n; i++) {
+
+        if (i == n - 1) return jumps;
+
+        maxReach = max(maxReach, i + arr[i]);
+        steps--;
+
+        if (steps == 0) {
+            jumps++;
+
+            if (i >= maxReach) return -1;
+
+            steps = maxReach - i;
+        }
+    }
+
+    return -1;
+}
